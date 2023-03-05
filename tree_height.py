0201bf352c75534dfa8310e2543622190b23934b
Dmitrijs Firosvs <125821414+DmitrijsFIrsovs@users.noreply.github.com>
2023-03-05 21:19:14 +0200

    Update tree_height.py

--- tree_height.py
+++ tree_height.py
@@ -2,14 +2,13 @@
 # Dmitrijs Firsovs 211RDB310
 
 import sys
-
+import threading
+import numpy as np
 
 def compute_height(n, parents):
-    
-    heights = [0] * n
-    
-    for i in range(n):
-        
+    heights = np.zeros(int(n))
+    max_height = 0
+    for i in range(int(n)):
         if heights[i] > 0:
             continue
         height = 0
@@ -20,19 +19,36 @@ def compute_height(n, parents):
                 break
             else:
                 height += 1
-                j = parents[j]
-       
+                j = int(parents[j])
         heights[i] = height
-   
-    return max(heights)
-
+        if height > max_height:
+            max_height = height
+    return max_height
 
 def main():
-    n = int(input("Enter the number of nodes in the tree: "))
-    parents = list(map(int, input("Enter the parent nodes of each node in the tree, separated by spaces: ").split()))
-    print("The maximum height of the tree is:", compute_height(n, parents))
+    input_method = input().strip()
+    n, parents = None, None
 
+    if input_method == "F":
+        file_dir = input().strip()
+        if str(file_dir[-1]) != "a":
+            try:
+                with open(f"./test/{file_dir}") as f:
+                    contents = f.readlines()
+                    n = contents[0].strip()
+                    if n:
+                        parents = contents[1].strip().split(" ")
+            except:
+                print("ERROR")
+    elif input_method == "I":
+        n = input().strip()
+        if n:
+            parents = input().strip().split(" ")
 
-sys.setrecursionlimit(10**7)
-threading.stack_size(2**27)
+    if n and parents:
+        height = compute_height(n, parents)
+        print(int(height))
+
+sys.setrecursionlimit(10 ** 7) 
+threading.stack_size(2 ** 27)  
 threading.Thread(target=main).start()
