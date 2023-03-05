1b55f61ca0a700cfa80ff2e5baf46354707ffb3f
Dmitrijs Firosvs <125821414+DmitrijsFIrsovs@users.noreply.github.com>
2023-03-05 20:47:32 +0200

    Update tree_height.py

--- tree_height.py
+++ tree_height.py
@@ -1,12 +1,13 @@
 # python3
-
-import argparse
+# Dmitrijs Firsovs 211RDB310
+import sys
+import threading
 import numpy as np
 
-def calculate_tree_height(n, parents):
-    heights = np.zeros(n)
+def compute_height(n, parents):
+    heights = np.zeros(int(n))
     max_height = 0
-    for i in range(n):
+    for i in range(int(n)):
         if heights[i] > 0:
             continue
         height = 0
@@ -17,43 +18,51 @@ def calculate_tree_height(n, parents):
                 break
             else:
                 height += 1
-                j = parents[j]
+                j = int(parents[j])
         heights[i] = height
         if height > max_height:
             max_height = height
     return max_height
 
-def read_input(input_file):
-    with open(input_file) as f:
-        n = int(f.readline().strip())
-        parents = np.fromstring(f.readline().strip(), sep=' ', dtype=int)
-        if n != parents.size:
-            raise ValueError("Invalid input: number of nodes and parent nodes don't match.")
-        return n, parents
+def input_from_keyboard():
+    n = input().strip()
+    if n:
+        parents = input().strip().split(" ")
+        if parents:
+            return n, parents
+    return None, None
 
-def main(input_file=None):
-    if input_file:
-        try:
-            n, parents = read_input(input_file)
-        except Exception as e:
-            print(f"Error: {str(e)}")
-            return
-    else:
-        parser = argparse.ArgumentParser(description="Calculate height of a tree.")
-        parser.add_argument("-n", type=int, required=True, help="Number of nodes in the tree")
-        parser.add_argument("-p", type=int, nargs='+', required=True, help="Parent nodes of each node in the tree")
-        args = parser.parse_args()
-        n = args.n
-        parents = np.array(args.p)
-        if n != parents.size:
-            print("Error: number of nodes and parent nodes don't match.")
-            return
-
+def input_from_file(file_dir):
     try:
-        height = calculate_tree_height(n, parents)
-        print(height)
-    except Exception as e:
-        print(f"Error: {str(e)}")
+        with open(f"./test/{file_dir}") as f:
+            contents = f.readlines()
+    except:
+        print("ERROR")
+        return None, None
 
-if __name__ == "__main__":
-    main()
+    n = contents[0].strip()
+    if n:
+        parents = contents[1].strip().split(" ")
+        if parents:
+            f.close()
+            return n, parents
+    return None, None
+
+def main():
+    input_method = input().strip()
+    if input_method == "F":
+        file_dir = input().strip()
+        if str(file_dir[-1]) != "a":
+            n, parents = input_from_file(file_dir)
+            if n and parents:
+                height = compute_height(n, parents)
+                print(int(height))
+    elif input_method == "I":
+        n, parents = input_from_keyboard()
+        if n and parents:
+            height = compute_height(n, parents)
+            print(int(height))
+
+sys.setrecursionlimit(10 ** 7)  # max depth of recursion
+threading.stack_size(2 ** 27)  # new thread will get stack of such size
+threading.Thread(target=main).start()
