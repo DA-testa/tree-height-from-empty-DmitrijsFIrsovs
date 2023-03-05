6f1d73eedd7634d2f72b9e6ad2d866ec98afe4d2
Dmitrijs Firosvs <125821414+DmitrijsFIrsovs@users.noreply.github.com>
2023-03-05 21:02:28 +0200

    Update tree_height.py

--- tree_height.py
+++ tree_height.py
@@ -1,67 +1,26 @@
 # python3
 # Dmitrijs Firsovs 211RDB310
+
 import sys
 import threading
 import numpy as np
 
 def compute_height(n, parents):
-heights = np.zeros(int(n))
-max_height = 0
-for i in range(int(n)):
-if heights[i] > 0:
-continue
-height = 0
-j = i
-while j != -1:
-if heights[j] > 0:
-height += heights[j]
-break
-else:
-height += 1
-j = int(parents[j])
-heights[i] = height
-if height > max_height:
-max_height = height
-return max_height
+    augst = np.zeros(int(n))
+    max_height = 0
+   
+    return max_height
 
-def input_from_keyboard():
-n = input().strip()
-if n:
-parents = input().strip().split(" ")
-if parents:
-return n, parents
-return None, None
 
-def input_from_file(file_dir):
-try:
-with open(f"./test/{file_dir}") as f:
-contents = f.readlines()
-except:
-print("ERROR")
-return None, None
-
-n = contents[0].strip()
-if n:
-    parents = contents[1].strip().split(" ")
-    if parents:
-        f.close()
-        return n, parents
-return None, None
 def main():
-input_method = input().strip()
-if input_method == "F":
-file_dir = input().strip()
-if str(file_dir[-1]) != "a":
-n, parents = input_from_file(file_dir)
-if n and parents:
-height = compute_height(n, parents)
-print(int(height))
-elif input_method == "I":
-n, parents = input_from_keyboard()
-if n and parents:
-height = compute_height(n, parents)
-print(int(height))
+         
+        
+        
+    pass
 
-sys.setrecursionlimit(10 ** 7)
-threading.stack_size(2 ** 27)
+
+sys.setrecursionlimit(10**7)  
+threading.stack_size(2**27)   
 threading.Thread(target=main).start()
+main()
+# print(numpy.array([1,2,3]))
