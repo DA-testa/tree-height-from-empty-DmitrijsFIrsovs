690421df280005e655c49b3815e540b87ecdc66e
Dmitrijs Firosvs <125821414+DmitrijsFIrsovs@users.noreply.github.com>
2023-03-05 21:10:37 +0200

    Update tree_height.py

--- tree_height.py
+++ tree_height.py
@@ -6,21 +6,79 @@ import threading
 import numpy as np
 
 def compute_height(n, parents):
-    augst = np.zero (int(n))
+    heights = np.zeros(int(n))
     max_height = 0
-   
+    for i in range(int(n)):
+        if heights[i] > 0:
+            continue
+        height = 0
+        j = i
+        while j != -1:
+            if heights[j] > 0:
+                height += heights[j]
+                break
+            else:
+                height += 1
+                j = int(parents[j])
+        heights[i] = height
+        if height > max_height:
+            max_height = height
     return max_height
 
+def read_input():
+   
+    n = input().strip()
+    if not n:
+        return None, None
+    parents = input().strip().split()
+    if len(parents) != int(n):
+        return None, None
+    return n, parents
+
+def read_input_file(filename):
+    try:
+        with open(filename) as f:
+            contents = f.readlines()
+    except FileNotFoundError:
+        print(f"Error: could not open file '{filename}'")
+        return None, None
+
+    n = contents[0].strip()
+    if not n:
+        return None, None
+    parents = contents[1].strip().split()
+    if len(parents) != int(n):
+        print("Error: invalid input format")
+        return None, None
+
+    return n, parents
 
 def main():
-         
-        
-        
-    pass
+   
+    input_method = input().strip().lower()
+    if input_method not in ("i", "f"):
+        print("Error: invalid input method")
+        return
 
+    
+    if input_method == "f":
+        filename = input().strip()
+        if not filename.endswith(".txt"):
+            print("Error: invalid input file format")
+            return
+        n, parents = read_input_file(filename)
+    else:
+        n, parents = read_input()
 
-sys.setrecursionlimit(10**7)  
-threading.stack_size(2**27)   
-threading.Thread(target=main).start()
-main()
-# print(numpy.array([1,2,3]))
+    if n is None or parents is None:
+        print("Error: invalid input format")
+        return
+
+    # 
+    height = compute_height(n, parents)
+    print(int(height))
+
+if __name__ == "__main__":
+    sys.setrecursionlimit(10 ** 7)
+    threading.stack_size(2 ** 27)
+    threading.Thread(target=main).start()
